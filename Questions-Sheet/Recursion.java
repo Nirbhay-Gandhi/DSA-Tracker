e2092ca5b4da702b5b64b45a8200e49b2f8f9af1
Nirbhay Gandhi <nsgandhi.in@gmail.com>
2025-04-06 16:19:52 +0530

    Update Recursion.java

--- Questions-Sheet/Recursion.java
+++ Questions-Sheet/Recursion.java
@@ -1,1 +1,30 @@
+public class Recursion {
+    
+    public static int factorialLoop(int n){
+        int fact = 1;
+        for(int i=1;i<=n;i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+   public static int factorial(int n)
+   {
+        /**sabse chota case, jiske liye tumhe lag rha hai, ki sahi se chal jaega*/
+        if(n == 0)
+            return 1;
+
+        /**using PMI, assume karlo ki chote problem kaa answer usne de diya hai*/
+        int smallOutput = factorial(n-1);
+        
+        /**ab jo small problem kaa op aaya hai, usko use karke apna op nikal lo*/
+        return n*smallOutput;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(factorial(5)); // 120
+        System.out.println(factorialLoop(5)); // 120
+    }
+}
 
